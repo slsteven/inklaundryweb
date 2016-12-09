3c932617ee3d9463e53a4920fda74fc64c2d7911
steven  lam <stevenlam@stevens-MacBook-Pro.local>
2016-12-09 13:48:22 -0800

    commented out db

--- server/models/order.js
+++ server/models/order.js
@@ -1,39 +1,39 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-var OrderSchema = new Schema({
-  order_Id: Number,
-  title: String,
-  invoice: String,
-  description: String,
-  school_info: {
-    school: String,
-    class: String,
-    grade_level: String,
-  },
-  apparel_details: {
-    brand: String,
-    model: String,
-    style: String,
-    color: String
-  },
-  size_breakdown: {
-    xsmall: Number,
-    small: Number,
-    medium: Number,
-    large: Number,
-    xlarge: Number,
-    xxlarge: Number,
-    xxxlarge: Number
-  },
-  print_locations: [],
-  images: {
-    main_image: String,
-    sub_image: String
-  },
-  price: Number,
-  image_url: String,
-  student_emails: [],
-})
+// var OrderSchema = new Schema({
+//   order_Id: Number,
+//   title: String,
+//   invoice: String,
+//   description: String,
+//   school_info: {
+//     school: String,
+//     class: String,
+//     grade_level: String,
+//   },
+//   apparel_details: {
+//     brand: String,
+//     model: String,
+//     style: String,
+//     color: String
+//   },
+//   size_breakdown: {
+//     xsmall: Number,
+//     small: Number,
+//     medium: Number,
+//     large: Number,
+//     xlarge: Number,
+//     xxlarge: Number,
+//     xxxlarge: Number
+//   },
+//   print_locations: [],
+//   images: {
+//     main_image: String,
+//     sub_image: String
+//   },
+//   price: Number,
+//   image_url: String,
+//   student_emails: [],
+// })
 
-module.exports = mongoose.model('Order', OrderSchema)
+// module.exports = mongoose.model('Order', OrderSchema)
--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,32 @@
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
-var Schema   = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var bcrypt   = require('bcrypt-nodejs');
+// var Schema   = mongoose.Schema;
 
-var UserSchema = new Schema({
-  name: {type: String },
-  // topics: [{type: Schema.Types.ObjectId, ref: 'Topic'}],
+// var UserSchema = new Schema({
+//   name: {type: String },
+//   // topics: [{type: Schema.Types.ObjectId, ref: 'Topic'}],
 
-  local: {
-    email: String,
-    password: String,
-  },
-  facebook: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  }
-});
+//   local: {
+//     email: String,
+//     password: String,
+//   },
+//   facebook: {
+//     id: String,
+//     token: String,
+//     email: String,
+//     name: String
+//   }
+// });
 
-//methods =============================
-  //generate hash
-UserSchema.methods.generateHash = function(password){
-  return
-};
+// //methods =============================
+//   //generate hash
+// UserSchema.methods.generateHash = function(password){
+//   return
+// };
 
-  // checking if password is valid
-UserSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+//   // checking if password is valid
+// UserSchema.methods.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.local.password);
+// };
 
-module.exports = mongoose.model('User', UserSchema)
+// // module.exports = mongoose.model('User', UserSchema)
